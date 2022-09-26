2a83b56f935c751056aba7406c88c95b6e0ae84d
Joelle Plow <joelleplow@pm.me>
2022-09-26 16:05:54 +0100

    WIP - TODO: Edit this commit!

--- routes.js
+++ routes.js
@@ -15,16 +15,24 @@ module.exports = function (app, opts) {
 
   app.post("/log.json", jsonParser, (req, res) => {
     const { name = `log at ${new Date().toUTCString()}`, content } = req.body;
+    console.log(req.body)
     let filepath = `${logPath}/${name}.json`;
     let n = 1;
     while (fs.existsSync(filepath)) filepath = `${logPath}/${name} ${n++}.json`;
 
+
+    if(content === undefined)
+      res.status(400).send({message: "Bad request: No content"})
+
+
+    console.log(`\n\nLogging: ${JSON.stringify(name)}:\n${JSON.stringify(content)}\n\n`)
+
     fs.writeFile(filepath, JSON.stringify(content), {encoding:'utf-8'}, (err) => {
       console.error(err)
       if(err)
-        res.sendStatus(500)
+        res.status(500).send({err})
       else
-        res.sendStatus(200)
+        res.status(200).send({success:true})
     });
   });
 };
